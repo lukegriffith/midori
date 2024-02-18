a7da5ff80183e11db49f1c099ccbf944575d4b15
Luke Griffith <lukemgriffith@gmail.com>
2024-02-18 16:20:04 +0000

    Adding install and correcting zsh_setup.sh

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 .PHONY: test
 test:
 	go test ./...
+
+
+.PHONY: install
+install:
+	go install cmd/journal/journal.go
--- zsh_setup.zsh
+++ zsh_setup.zsh
@@ -1,9 +1,9 @@
 # Function to log command into database
 log_command() {
     local cmd="${1[0, -2]}"
-    go run ~/dev/midori/cmd/journal/journal.go add "$cmd" $PWD "command"
-
+    if [[ -n "$cmd" ]]; then
+        journal add "$cmd" $PWD "command"
+    fi
 }
 
 add-zsh-hook zshaddhistory log_command
-
